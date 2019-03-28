46e0a3341839d879299ce2cde34aabf66da882b2
Dheeraj <dheeraj@bluejeansnet.com>
2019-03-28 21:44:12 +0530

    2nd commit

--- src/index.js
+++ src/index.js
@@ -5,8 +5,23 @@ import LanguageObject from './js/language.json';
 
 var meetingid = "10";
 var Client;
-
+var didIMakeChange = false;
 var pending;
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 
 function updateCode(){
 	var options = getValueObject();
@@ -14,62 +29,52 @@ function updateCode(){
 }
 
 
-editor.on("change", function() {
-  clearTimeout(pending);
-  pending = setTimeout(updateCode, 400);
-});
+editor.on("paste", 
+	debounce(function(){updateCode();},500) 
+);
+
+editor.on("keypress", 
+	debounce(function(){updateCode();},500) 
+);
 
 var languageInput = document.getElementById('language-select');
 languageInput.onchange = updateCode;
 
-function changeUIonUpdate(options){
-  var languageId = document.getElementById('language-select').value;
-  var code = editor.getValue();
-	if(languageId !== options.language_id){
-		languageInput.value = options.language_id;
-	}
-	if(code !== options.source_code){
-		editor.setValue(options.source_code);
-	}
-}
 
-ws("localhost:4100/code-editor", meetingid)
-	.then((client) => {
+
+ws("10.5.22.216:4100/code-editor", meetingid)
+	.then((client, data, someBlah) => {
 	    Client = client;
-	    Client.startIDE = function() {
-		    this.emit("a2mevent.notification.message", {
-		        meetingId: meetingid,
-		        message: {
-		            "ACTION": "IDE_STARTED"
+
+
+		if(someBlah) {
+
+			Client.on('a2mevent.notification.message', (data) => {
+                if(data.action == "CHANGE_CODE") {
+		    		changeUIonUpdate(data.message);
 		        }
-		    });
-		}
+		        if(data.action == "SHOW_RESULT") {
+		        	// change result ui block
+		        } 
 
-
-	    Client.stopIDE = function() {
-		    this.emit("a2mevent.notification.message", {
-		        meetingId: meetingid,
-		        message: {
-		            "ACTION": "IDE_STOPPED"
-		        }
-		    });
-		}
-
-
-		Client.recieveCode = function(options) {
-		    // CALL API
-			console.log(options);
+		    	console.log("receiveCode", data.message);
+            });
+		    
 		}
 
 		Client.changeCode = function(options) {
-		    // CALL API
-			console.log(options);
+		    console.log("changeCode", options);
+		    Client.emit("a2mevent.notification.message", {
+		        meetingId: meetingid,
+		        action : "CHANGE_CODE",
+		        message: options
+		    });
 		}
 
 		Client.uploadCode = function(options) {
 		    // CALL API
-		    changeUIonUpdate(options);
-			console.log(options);
+
+			console.log("uploadCode", options);
 		}
 	})
 	.catch((err) => {
