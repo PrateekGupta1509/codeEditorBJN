570677b0164b4e571fd8990e0046e38ea78d73d0
Dheeraj <dheeraj@bluejeansnet.com>
2019-03-28 22:26:40 +0530

    Working Websockets

--- src/js/ws-client.js
+++ src/js/ws-client.js
@@ -4,37 +4,6 @@ var Client;
 ws("10.5.22.216:4100/code-editor", meetingid)
 .then((client) => {
     Client = client;
-    Client.startIDE = function() {
-
-    this.emit("a2mevent.notification.message", {
-        meetingId: meetingid,
-        message: {
-            "ACTION": "IDE_STARTED"
-        }
-    });
-}
-
-
-    Client.stopIDE = function() {
-
-    this.emit("a2mevent.notification.message", {
-        meetingId: meetingid,
-        message: {
-            "ACTION": "IDE_STOPPED"
-        }
-    });
-}
-
-
-
-
-    Client.uploadCode = function() {
-    // CALL API
-}
-
-
-
-
 
 })
 .catch((err) => {
